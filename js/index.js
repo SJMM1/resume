d37c00f60caff7ef544db6a79c299753fbdc8183
SJMM1 <108396668+SJMM1@users.noreply.github.com>
2022-09-27 18:13:53 +0900

    Add files via upload

--- js/index.js
+++ js/index.js
@@ -1,6 +1,3 @@
-// let d_width = 0; // 브라우저 가로
-// let d_height = 0; // 문서 전체의 높이
-
 function tmp() {
     // container의 가로사이즈(화면가로 * box 개수)
     let con_width = $(window).outerWidth() * $('.box').length; 
@@ -19,23 +16,7 @@ function tmp() {
         float: 'left'
     });
 
-    $('.room').css({
-        width: $('.box').width() - $('.items').width()
-    })
-
-    // box들을 위로 끌어올렸기 때문에 body의 높이는 100vh나 마찬가지인 상태. 
-    // 그래서 억지로 전체 box들의 세로크기 만큼 body에 줘야한다.(스크롤 내리기위함) 
-    // 이때 높이는 가로영역의 비율과 동일하게 준다. (이후 리미트를 주게 됨으로써 비율의 값이 정해진다.)
-    $('body').css({
-        height: '100vh'
-    });
-
-    // let w_width = $(window).width(); // 화면의 가로값
-    // let w_height = $(window).height() // 화면의 세로값
-
-    // 스크롤 될때의 리미트
-    // d_width = con_width - w_width; // 전체 가로값 - 현재 화면의 가로값
-    // d_height = $('body').height() - w_height // 전체 세로값 - 현재 화면의 세로값
+    $('.room').css({ width: $('.box').width() - $('.items').width() })
 }
 
 tmp();
@@ -44,72 +25,188 @@ $(window).resize(function(){
 })
 
 let chk = true;
+
 $('.box').on('mousewheel DOMMouseScroll', function(){
-    if(chk) {
-        // 휠 일정시간동안 막기
-        chk = false;
-        setTimeout(function(){
-            chk = true;
-        }, 500)
-
-        // 휠 방향 감지(아래: -120, 위: 120)
-        let w_delta = event.wheelDelta / 120;
-        
-        // 휠 아래로
-        if(w_delta < 0 && $(this).next().length > 0) {
-            $('.container').animate({
-                left: -$(this).width() * ($(this).index()+1)
+    if(!$('.pop').hasClass('on')) {
+        if(chk) {
+            // 휠 일정시간동안 막기
+            chk = false;
+            setTimeout(function(){
+                chk = true;
             }, 500)
+    
+            // 휠 방향 감지(아래: -120, 위: 120)
+            let w_delta = event.wheelDelta / 120;
+            
+            // 휠 아래로s
+            if(w_delta < 0 && $(this).next().length > 0) {
+                $('.container').animate({
+                    left: -$(this).width() * ($(this).index()+1)
+                }, 500)
+            }
+            // 휠 위로
+            else if(w_delta > 0 && $(this).prev().length > 0) {
+                $('.container').animate({
+                    left: -$(this).width() * ($(this).index()-1)
+                }, 500)
+            }
         }
-        // 휠 위로
-        else if(w_delta > 0 && $(this).prev().length > 0) {
-            $('.container').animate({
-                left: -$(this).width() * ($(this).index()-1)
-            }, 500)
-        }
-    }
+    } 
 });
-
 
 $('img[usemap]').rwdImageMaps();
 
-
-
-
-// gallery
-$('#gallery').on('click',function(){
+// 팝업창
+$('.maparea').on('click', function(){
+    $(this).next().addClass('on')
     $(this).parent().append(`<div class='backcolor'></div>`)
-    $('.backcolor').css({
-        position: 'fixed',
-        top: 0,
-        left: 0,
-        right: 0,
-        bottom: 0,
-        background: 'rgba(0,0,0,0.5)'
-    })
-    $(".book_wrap").addClass('on')
+    
+    // room1
+    if ($(this).hasClass('about')) {
+        
+    }
 
-    let book = `<div id="flipbook">
-                    <div class="hard">PORTFOLIO</div>
-                    <div class="hard"></div>
-                    <div>Page 1</div>
-                    <div>Page 2</div>
-                    <div>Page 3</div>
-                    <div>Page 4</div>
-                    <div class="hard"></div>
-                    <div class="hard"></div>
-                </div>`
-    $('.book_wrap').append(book)
-    $('#flipbook').turn({
-        width: 800,
-        height: 600
-    });
+    // room2
+    if ($(this).hasClass('skill')) {
+        let chart = `<canvas id="skill"></canvas>`
+        $('.skillBox > div').append(chart)
+        new Chart($('#skill'), {
+            type: 'polarArea',
+            data: {
+                labels: ['HTML', 'CSS', 'JavaScript', 'JQuery', 'PHOTOSHOP', 'ILLUSTRATOR'],
+                datasets: [{
+                    label: 'SKILL',
+                    data: [85, 75, 65, 70, 60, 60],
+                    backgroundColor: [
+                        'rgba(255, 99, 132, 0.3)',
+                        'rgba(54, 162, 235, 0.3)',
+                        'rgba(255, 206, 86, 0.3)',
+                        'rgba(75, 192, 192, 0.3)',
+                        'rgba(153, 102, 255, 0.3)',
+                        'rgba(255, 159, 64, 0.3)'
+                    ],
+                    borderColor: [
+                        'rgba(255, 99, 132, 1)',
+                        'rgba(54, 162, 235, 1)',
+                        'rgba(255, 206, 86, 1)',
+                        'rgba(75, 192, 192, 1)',
+                        'rgba(153, 102, 255, 1)',
+                        'rgba(255, 159, 64, 1)'
+                    ],
+                    borderWidth: 1
+                }]
+            },
+            options: {
+                scales: {
+                    r: {
+                        pointLabels: {
+                            display: true,
+                            centerPointLabels: true,
+                            font: {
+                                size: 16
+                            }
+                        },
+                        min: 0,
+                        max: 100
+                    }
+                },
+                plugins: {
+                    legend: {
+                        position: 'top',
+                    },
+                    title: {
+                        display: true,
+                        text: 'SKILL',
+                        font: {
+                            size: 50,
+                            weight: 'bold'
+                        }
+                    }
+                }
+            }
+        });
+    }
+
+    // room3
+    if ($(this).hasClass('portfolio')) {
+        let book = 
+        `<div id="flipbook">
+            <div class="hard">
+                <h1>PORTFOLIO</h1>
+                <div class="scene">
+                    <div class="upper">
+                    <div class="moon">
+                        <div class="crater1"></div>
+                        <div class="crater2"></div>
+                    </div>
+                    <div class="star1"></div>
+                    <div class="star2"></div>
+                    <div class="star3"></div>
+                    <div class="cloud1">
+                        <div class="circle"></div>
+                        <div class="filler"></div>
+                    </div>
+                    <div class="cloud2">
+                        <div class="circle"></div>
+                        <div class="filler"></div>
+                    </div>
+                    <div class="tail">
+                        <div class="left"></div>
+                        <div class="right"></div>
+                        <div class="body"></div>
+                    </div>
+                    <div class="drop"></div>
+                    </div>
+                    <div class="lower">
+                    <div class="whale">
+                        <div class="eye"></div>
+                        <div class="detail1">
+                        <div class="detail2"></div>
+                        </div>
+                    </div>
+                    <div class="fin"></div>
+                    </div>
+                </div>
+            </div>
+            <div class="hard"></div>
+            <div>Page 1</div>
+            <div>Page 2</div>
+            <div>Page 3</div>
+            <div>Page 4</div>
+            <div class="hard"></div>
+            <div class="hard"></div>
+        </div>`
+        $('.portfolioBox').append(book)
+        $('#flipbook').turn({
+            width: $('.room3').width() * 0.85,
+            height: $('.room3').height() * 0.85
+        });
+    }
+
+    
 })
 
-$('.book_close').on('click', function(){
-    $(".book_wrap").removeClass('on')
+// 닫기
+$('.close').on('click', function(){
+    $(this).parent().removeClass('on')
     $('.backcolor').remove()
-    $('#flipbook').remove()
+    if($(this).parent().parent().hasClass('room2')){
+        $('#skill').remove()
+    }
+    if ($(this).parent().parent().hasClass('room3')){
+        $('#flipbook').remove()
+    }
 })
+$('.close').hover(
+    function(){
+        $(this).addClass('fa-spin');
+    },
+    function(){
+        $(this).removeClass('fa-spin');
+    }
+);
 
 
+
+
+	
